4e26cd67f99ce964be89fde23c6e3ef3c5cafcec
Kevin Matthews <kmatthews@cyberdata.net>
2016-05-03 12:02:16 -0700

    Rewrite NotifierWrapper tests
    Split responsibilies:
      set behavior on file changed
      block until file changed
    
    Pass behavior as an initialization parameter.

--- src/notifier_wrapper.rb
+++ src/notifier_wrapper.rb
@@ -3,17 +3,19 @@
 require 'rb-inotify'
 
 class NotifierWrapper
-  attr_reader :notifier
-  def initialize(notifier: INotify::Notifier.new)
+  attr_reader :notifier, :behavior
+  def initialize(notifier: INotify::Notifier.new, behavior: nil)
     @notifier = notifier
+    @behavior = behavior
   end
 
-  def do_something_when_file_changes(filename)
-    notifier.watch(filename, :modify) do |event|
-      # This is executed when the file is modified.
-      # How can I simulate this?
-      puts "Your file has changed!"
+  def set_action_on_file_changed(filename)
+    notifier.watch(filename, :modify) do
+      behavior.on_file_changed
     end
+  end
+
+  def block_until_file_changes
     notifier.process
   end
 end
--- test/test_notifier_wrapper.rb
+++ test/test_notifier_wrapper.rb
@@ -4,6 +4,12 @@ require 'minitest/autorun'
 require_relative '../src/notifier_wrapper.rb'
 
 describe NotifierWrapper do
+  class TestNotifier < INotify::Notifier
+    def watch(filename, action)
+      yield
+    end
+  end
+
   it 'uses inotify by default' do
     wrapper = NotifierWrapper.new
     wrapper.notifier.must_be_instance_of(INotify::Notifier)
@@ -11,13 +17,23 @@ describe NotifierWrapper do
 
   it 'can detect a change to a file' do
     filename = 'sample_file.txt'
+    mock_behavior = MiniTest::Mock.new
+    wrapper = NotifierWrapper.new(notifier: TestNotifier.new, behavior: mock_behavior)
+
+    mock_behavior.expect(:on_file_changed, :not_using_return_value_yet, [])
+
+    wrapper.set_action_on_file_changed(filename)
+
+    mock_behavior.verify
+  end
+
+  it 'blocks until the first time the file changes' do
     mock_notifier = MiniTest::Mock.new
     wrapper = NotifierWrapper.new(notifier: mock_notifier)
 
-    mock_notifier.expect(:watch, :figure_out_what_to_return, [filename, :modify])
-    mock_notifier.expect(:process, nil)
+    mock_notifier.expect(:process, nil, [])
 
-    wrapper.do_something_when_file_changes(filename)
+    wrapper.block_until_file_changes
 
     mock_notifier.verify
   end
