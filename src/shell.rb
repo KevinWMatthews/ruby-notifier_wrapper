8e077ef2d6de136751d097b01a023c1bed062893
Kevin Matthews <kmatthews@cyberdata.net>
2016-05-03 17:37:13 -0700

    Add Shell Wrapper and Open4 shell

--- src/shell.rb
+++ src/shell.rb
@@ -1,1 +1,9 @@
 #!/usr/bin/env ruby
+
+require 'open4'
+
+class ShellWrapper
+  def execute(command)
+    Open4.popen4(command)
+  end
+end
--- test/test_shell.rb
+++ test/test_shell.rb
@@ -1,10 +1,17 @@
 #!/usr/bin/env ruby
 
 require 'minitest/autorun'
+require 'spy'
 require_relative '../src/shell.rb'
 
-describe 'Shell' do
-  it 'can fail' do
-    flunk
+describe ShellWrapper do
+  it 'can send a command using Open4' do
+    command = 'sample shell command'
+    shell = ShellWrapper.new
+    spy = Spy.on(Open4, :popen4)
+
+    shell.execute(command)
+
+    spy.has_been_called_with?(command).must_be :==, true
   end
 end
