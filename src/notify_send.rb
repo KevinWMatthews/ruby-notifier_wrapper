5aac8e2876422c1adbaf4d881abf80ac173bca16
Kevin Matthews <kmatthews@cyberdata.net>
2016-05-03 17:54:29 -0700

    Refactor existing code to make modification easier

--- src/notify_send.rb
+++ src/notify_send.rb
@@ -8,7 +8,18 @@ class NotifySend
     @shell = shell
   end
 
-  def send_notification(summary)
-    shell.execute("notify-send \"#{summary}\"")
+  def send_notification(summary, body: nil)
+    shell.execute( create_notification(summary, body) )
   end
+
+  private
+    def create_notification(summary, body)
+      "#{NOTIFY_SEND} #{format_text(summary)}"
+    end
+
+    def format_text(text)
+      "\"#{text}\""
+    end
+
+    NOTIFY_SEND = "notify-send"
 end
