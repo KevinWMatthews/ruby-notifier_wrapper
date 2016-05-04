c34fe6c7bdeb91f0d0c5ce3866e07539e21175f5
Kevin Matthews <kmatthews@cyberdata.net>
2016-05-03 18:00:11 -0700

    Update NotifySend demo script

--- scripts/demo_notify_send.rb
+++ scripts/demo_notify_send.rb
@@ -4,3 +4,4 @@ require_relative '../src/notify_send.rb'
 
 notifier = NotifySend.new
 notifier.send_notification("Hello world!")
+notifier.send_notification("Summary of shrubbery", body: "Ni! Ni! Ni!")
