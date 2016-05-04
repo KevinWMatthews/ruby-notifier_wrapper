6e475588af626a8b9fe139a63082ee2d7948ac58
Kevin Matthews <kmatthews@cyberdata.net>
2016-05-03 17:19:18 -0700

    Add DesktopNotification behavior

--- src/behavior.rb
+++ src/behavior.rb
@@ -5,3 +5,15 @@ class DoNothing
     return
   end
 end
+
+class DesktopNotification
+  attr_reader :desktop_notifier, :message
+  def initialize(message: nil, desktop_notifier: nil)
+    @desktop_notifier = desktop_notifier
+    @message = message
+  end
+
+  def on_file_changed
+    desktop_notifier.send_notification(message)
+  end
+end
--- test/test_behavior.rb
+++ test/test_behavior.rb
@@ -9,3 +9,17 @@ describe DoNothing do
     behavior.on_file_changed
   end
 end
+
+describe DesktopNotification do
+  it 'sends a desktop notification' do
+    message = "You have been notified!"
+    mock_notifier = MiniTest::Mock.new
+    behavior = DesktopNotification.new(message: message, desktop_notifier: mock_notifier)
+
+    mock_notifier.expect(:send_notification, nil, [message])
+
+    behavior.on_file_changed
+
+    mock_notifier.verify
+  end
+end
