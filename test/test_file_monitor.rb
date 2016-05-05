679c33b70d06e07701d34c096b4dd9dc310ab92c
Kevin Matthews <kmatthews@cyberdata.net>
2016-05-05 15:43:16 -0700

    Sketch test format for blocking indefinitely

--- test/test_file_monitor.rb
+++ test/test_file_monitor.rb
@@ -24,30 +24,38 @@ describe FileMonitor do
     wrapper.monitor.must_be_instance_of(INotify::Notifier)
   end
 
-  it 'blocks until the file changes' do
-    filename = 'sample_file.txt'
-    mock_notifier = MiniTest::Mock.new
-    wrapper = FileMonitor.new(monitor: TestNotifier.new(mock_notifier))
+  describe 'when blocking once' do
+    it 'blocks until the file changes' do
+      filename = 'sample_file.txt'
+      mock_notifier = MiniTest::Mock.new
+      wrapper = FileMonitor.new(monitor: TestNotifier.new(mock_notifier))
 
-    mock_notifier.expect(:process, nil, [])
+      mock_notifier.expect(:process, nil, [])
 
-    wrapper.block_until_file_changes(filename)
+      wrapper.block_until_file_changes(filename)
 
-    mock_notifier.verify
+      mock_notifier.verify
+    end
+
+    it 'yields to a block when the file changes' do
+      filename = 'sample_file.txt'
+      mock_notifier = MiniTest::Mock.new
+      mock_behavior = MiniTest::Mock.new
+      wrapper = FileMonitor.new(monitor: TestNotifier.new(mock_notifier))
+
+      mock_behavior.expect(:on_file_changed, :not_using_return_value_yet, [])
+      mock_notifier.expect(:process, nil, [])
+
+      wrapper.block_until_file_changes(filename) { mock_behavior.on_file_changed }
+
+      mock_behavior.verify
+      mock_notifier.verify
+    end
   end
 
-  it 'yields to a block when the file changes' do
-    filename = 'sample_file.txt'
-    mock_notifier = MiniTest::Mock.new
-    mock_behavior = MiniTest::Mock.new
-    wrapper = FileMonitor.new(monitor: TestNotifier.new(mock_notifier))
-
-    mock_behavior.expect(:on_file_changed, :not_using_return_value_yet, [])
-    mock_notifier.expect(:process, nil, [])
-
-    wrapper.block_until_file_changes(filename) { mock_behavior.on_file_changed }
-
-    mock_behavior.verify
-    mock_notifier.verify
+  describe 'when blocking indefinitely' do
+    it 'can block until a string match is detected' do
+      skip
+    end
   end
 end
