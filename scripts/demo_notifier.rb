f370ccbef72a84c39cbaa6084d70a21387f3cfea
Kevin Matthews <kmatthews@cyberdata.net>
2016-05-05 15:03:43 -0700

    Update demo script to use new object structure
    Also rename the file

--- scripts/demo_notifier.rb
+++ scripts/demo_notifier.rb
@@ -6,7 +6,9 @@ require_relative '../src/behavior.rb'
 filename = 'sample_file.txt'
 message_summary = "File changed detected!"
 message_body = "#{filename} has been modified."
-notifier = NotifierWrapper.new( behavior: DesktopNotification.new(message_summary, body: message_body) )
+notification = NotifySend.new(summary: message_summary, body: message_body)
+behavior = DesktopNotification.new(notification: notification)
+notifier = NotifierWrapper.new(behavior: behavior)
 
 notifier.set_action_on_file_changed(filename)
 notifier.block_until_file_changes
