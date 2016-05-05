378fa2d7b08e497d4f069d16be34475a0bb11ba7
Kevin Matthews <kmatthews@cyberdata.net>
2016-05-05 15:34:57 -0700

    Lingering renaming

--- scripts/demo_file_monitor.rb
+++ scripts/demo_file_monitor.rb
@@ -7,6 +7,6 @@ filename = 'sample_file.txt'
 message_summary = "File changed detected!"
 message_body = "#{filename} has been modified."
 notification = NotifySend.new(summary: message_summary, body: message_body)
-notifier = FileMonitor.new
+file_monitor = FileMonitor.new
 
-notifier.block_until_file_changes(filename) { notification.send_notification }
+file_monitor.block_until_file_changes(filename) { notification.send_notification }
